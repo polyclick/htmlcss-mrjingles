f53e09ea78610ca76c12ca6619fb5999e7fbc7f1
Bart Claessens <bart.cla@gmail.com>
2015-05-25 15:29:05 +0200

    Legal stuff

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -27,4 +27,36 @@ $(function() {
 
     return false;
   });
+
+  // more info button
+  $('.more-info').click(function(event){
+    event.preventDefault();
+    var height = $('.more-info-panel').height() > 0 ? 0 : $('.more-info-panel-content').height();
+    $('.more-info-panel').height(height);
+    return false;
+  });
+
+  // legal popup
+  $('.legal-link').click(function(event){
+    event.preventDefault();
+    openCenteredPopup('legal.html', 'Voorwaarden en bepalingen', 550, 600);
+    return false;
+  });
 });
+
+function openCenteredPopup(url, title, w, h) {
+  var dualScreenLeft = window.screenLeft != undefined ? window.screenLeft : screen.left;
+  var dualScreenTop = window.screenTop != undefined ? window.screenTop : screen.top;
+
+  width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
+  height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
+
+  var left = ((width / 2) - (w / 2)) + dualScreenLeft;
+  var top = ((height / 2) - (h / 2)) + dualScreenTop;
+  var newWindow = window.open(url, title, 'scrollbars=yes, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
+
+  // Puts focus on the newWindow
+  if (window.focus) {
+      newWindow.focus();
+  }
+}
