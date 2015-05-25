7b7e935dc03883b6dd14944b2a55412cd173833a
Bart Claessens <bart.cla@gmail.com>
2015-05-25 14:50:56 +0200

    Next section, scroll animation between sections

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,4 +1,22 @@
-var date = new Date();
-$('[year]').each(function(){
-  $(this).html(date.getFullYear());
+$(function() {
+
+  // set current year in copyright
+  var date = new Date();
+  $('[year]').each(function(){
+    $(this).html(date.getFullYear());
+  });
+
+  // next section buttons
+  $('[next-section]').click(function(event){
+    event.preventDefault();
+    var nextSection = $(this).attr('next-section');
+    var sectionElement = $('section.' + nextSection);
+    console.log($(sectionElement).outerHeight() > $(window).height());
+    var diff = $(window).height() - $(sectionElement).outerHeight();
+    var offset = diff > 0 ? $(sectionElement).offset().top - (diff / 2) : $(sectionElement).offset().top;
+    $('html, body').animate({
+        scrollTop:offset
+    }, 750);
+    return false;
+  });
 });
