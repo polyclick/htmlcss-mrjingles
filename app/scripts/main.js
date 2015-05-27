f6f61c6b56fcc709de8c1e8f76bbd3ff89f3d27d
Bart Claessens <bart@revolver.be>
2015-05-27 12:37:55 +0200

    Styling & copy

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,3 +1,5 @@
+'use strict';
+
 $(function() {
 
   // set current year in copyright
@@ -7,12 +9,12 @@ $(function() {
   });
 
   // next section buttons
-  var page = $("html, body");
+  var page = $('html, body');
   $('[next-section] .next-button').click(function(event){
     event.preventDefault();
 
     // stop scroll animation when user is scrolling manually listener
-    page.on("scroll mousedown wheel DOMMouseScroll mousewheel keyup touchmove", function(){
+    page.on('scroll mousedown wheel DOMMouseScroll mousewheel keyup touchmove', function(){
       page.stop();
     });
 
@@ -22,7 +24,7 @@ $(function() {
     var offset = diff > 0 ? $(sectionElement).offset().top - (diff / 2) : $(sectionElement).offset().top;
 
     page.animate({scrollTop: offset }, 750, function(){
-      page.off("scroll mousedown wheel DOMMouseScroll mousewheel keyup touchmove");
+      page.off('scroll mousedown wheel DOMMouseScroll mousewheel keyup touchmove');
     });
 
     return false;
@@ -31,7 +33,7 @@ $(function() {
   // more info button
   $('.more-info').click(function(event){
     event.preventDefault();
-    var height = $('.more-info-panel').height() > 0 ? 0 : $('.more-info-panel-content').height();
+    var height = $('.more-info-panel').height() > 0 ? 0 : $('.more-info-panel-content').outerHeight(true);
     $('.more-info-panel').height(height);
     return false;
   });
@@ -42,14 +44,19 @@ $(function() {
     openCenteredPopup('legal.html', 'Voorwaarden en bepalingen', 550, 600);
     return false;
   });
+
+  // resize logic
+  $(window).resize(function(){
+    $('.more-info-panel').height(0);
+  });
 });
 
 function openCenteredPopup(url, title, w, h) {
-  var dualScreenLeft = window.screenLeft != undefined ? window.screenLeft : screen.left;
-  var dualScreenTop = window.screenTop != undefined ? window.screenTop : screen.top;
+  var dualScreenLeft = window.screenLeft !== undefined ? window.screenLeft : screen.left;
+  var dualScreenTop = window.screenTop !== undefined ? window.screenTop : screen.top;
 
-  width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
-  height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
+  var width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
+  var height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
 
   var left = ((width / 2) - (w / 2)) + dualScreenLeft;
   var top = ((height / 2) - (h / 2)) + dualScreenTop;
