a1520c67ba7c52bc3993d296e37a89f0cb8f5e81
Bart Claessens <bart@revolver.be>
2015-05-27 17:36:38 +0200

    Mobile menu, styling

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -2,16 +2,29 @@
 
 $(function() {
 
-  // set current year in copyright
-  var date = new Date();
-  $('[year]').each(function(){
-    $(this).html(date.getFullYear());
-  });
-
-  // next section buttons
+  // variables
   var page = $('html, body');
-  $('[next-section] .next-button').click(function(event){
-    event.preventDefault();
+
+  // utility function: open popup in center of the screen
+  function openCenteredPopup(url, title, w, h) {
+    var dualScreenLeft = window.screenLeft !== undefined ? window.screenLeft : screen.left;
+    var dualScreenTop = window.screenTop !== undefined ? window.screenTop : screen.top;
+
+    var width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
+    var height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
+
+    var left = ((width / 2) - (w / 2)) + dualScreenLeft;
+    var top = ((height / 2) - (h / 2)) + dualScreenTop;
+    var newWindow = window.open(url, title, 'scrollbars=yes, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
+
+    // Puts focus on the newWindow
+    if (window.focus) {
+        newWindow.focus();
+    }
+  }
+
+  // utility function: navigate to a specific section
+  function navigateToSection(sectionElement) {
 
     // stop scroll animation when user is scrolling manually listener
     page.on('scroll mousedown wheel DOMMouseScroll mousewheel keyup touchmove', function(){
@@ -19,14 +32,38 @@ $(function() {
     });
 
     // start scroll animation and remove manual scroll listener on complete
-    var sectionElement = $('section.' + $(this).closest('[next-section]').attr('next-section'));
     var diff = $(window).height() - $(sectionElement).outerHeight();
     var offset = diff > 0 ? $(sectionElement).offset().top - (diff / 2) : $(sectionElement).offset().top;
 
     page.animate({scrollTop: offset }, 750, function(){
       page.off('scroll mousedown wheel DOMMouseScroll mousewheel keyup touchmove');
     });
+  }
 
+  // set current year in copyright
+  var date = new Date();
+  $('[year]').each(function(){
+    $(this).html(date.getFullYear());
+  });
+
+  // menu buttons
+  $('.nav li').click(function(event){
+    event.preventDefault();
+    var section = $('section.' + $(this).attr('section'));
+
+    // wait for menu to close first
+    setTimeout(function(){
+      navigateToSection(section);
+    }, 0);
+
+    return false;
+  });
+
+  // next section buttons
+  $('[next-section] .next-button').click(function(event){
+    event.preventDefault();
+    var section = $('section.' + $(this).closest('[next-section]').attr('next-section'));
+    navigateToSection(section);
     return false;
   });
 
@@ -45,25 +82,15 @@ $(function() {
     return false;
   });
 
+  // mobile menu
+  $('.main, header ul li').click(function(event){
+    event.preventDefault();
+    $('header').toggleClass('menu-opened');
+    return false;
+  });
+
   // resize logic
   $(window).resize(function(){
     $('.more-info-panel').height(0);
   });
 });
-
-function openCenteredPopup(url, title, w, h) {
-  var dualScreenLeft = window.screenLeft !== undefined ? window.screenLeft : screen.left;
-  var dualScreenTop = window.screenTop !== undefined ? window.screenTop : screen.top;
-
-  var width = window.innerWidth ? window.innerWidth : document.documentElement.clientWidth ? document.documentElement.clientWidth : screen.width;
-  var height = window.innerHeight ? window.innerHeight : document.documentElement.clientHeight ? document.documentElement.clientHeight : screen.height;
-
-  var left = ((width / 2) - (w / 2)) + dualScreenLeft;
-  var top = ((height / 2) - (h / 2)) + dualScreenTop;
-  var newWindow = window.open(url, title, 'scrollbars=yes, width=' + w + ', height=' + h + ', top=' + top + ', left=' + left);
-
-  // Puts focus on the newWindow
-  if (window.focus) {
-      newWindow.focus();
-  }
-}
